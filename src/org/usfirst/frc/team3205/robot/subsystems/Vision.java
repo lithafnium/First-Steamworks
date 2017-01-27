d92f8bbf72b3aa37128fddaf78793b8e586612e4
lithafnium <lithafnium2000@gmail.com>
2017-01-27 16:14:39 -0500

    vision code stuff

--- src/org/usfirst/frc/team3205/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team3205/robot/subsystems/Vision.java
@@ -1,6 +1,13 @@
 package org.usfirst.frc.team3205.robot.subsystems;
 
+import org.opencv.imgproc.Imgproc;
+
+import com.ni.vision.NIVision.Rect;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.vision.VisionThread;
 
 /**
  *
@@ -9,11 +16,40 @@ public class Vision extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	private static final int IMG_WIDTH = 320;
+	private static final int IMG_HEIGHT = 240;
 	
+	private VisionThread visionThread;
+	private double centerX = 0.0;
+	//private RobotDrive drive;
+	
+	private final Object imgLock = new Object();
+	public Vision(){
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+	    camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+	    
+	    visionThread = new VisionThread(camera, new Pipeline(), pipeline -> {
+	        if (!pipeline.filterContoursOutput().isEmpty()) {
+	            org.opencv.core.Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+	            synchronized (imgLock) {
+	                centerX = r.x + (r.width / 2);
+	            }
+	        }
+	    });
+	    visionThread.start();
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    public void calculate(){
+    	double centerX;
+    	synchronized (imgLock) {
+    		centerX = this.centerX;
+    	}
+    	double turn = centerX - (IMG_WIDTH / 2);
+    	//drive.arcadeDrive(-0.6, turn * 0.005);
     }
 }
 
