105a6420fc6ad68100c4fdc883a3954d17e99590
lithafnium <lithafnium2000@gmail.com>
2017-02-01 14:45:00 -0500

    testing

--- src/org/usfirst/frc/team3205/robot/subsystems/Hopper.java
+++ src/org/usfirst/frc/team3205/robot/subsystems/Hopper.java
@@ -27,11 +27,8 @@ public class Hopper extends Subsystem {
 	public Hopper(){
 		intake = new Talon(RobotMap.INTAKE); 
 		hopperOne = new Servo(RobotMap.INTAKE_FLAP_OUT); 
-<<<<<<< HEAD
 		flapOut = new DigitalInput(RobotMap.IS_FLAP_OUT); 
-=======
-		flapOut = new DigitalInput(RobotMap.IF_FLAP_OUT); 
->>>>>>> 8f6f41ddc58ef836844b6c1be994522da710c14f
+
 	}
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
@@ -58,7 +55,7 @@ public class Hopper extends Subsystem {
     
     public void updateSmartDashboard(){
     	//SmartDashboard.putNumber("Drive Encoder One", );
-    	SmartDashboard.putBoolean("Drive Encoder Two", isFlapOut());
+    	SmartDashboard.putBoolean("Flapout", isFlapOut());
     }
     
 }
